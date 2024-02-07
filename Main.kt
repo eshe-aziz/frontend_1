ba01bec2a24994a9811549a8cd594d06345aab8b
eshe-aziz <eshehaledhaziz@gmail.com>
2024-02-07 08:12:15 +0300

    added kotlin files

---
+++ Main.kt
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var fullName: String = "Eshe Aziz"
+    println(fullName)
+
+    var age: Byte = 20
+    println(age)
+
+    var phoneNumber: String = "0716752733"
+    println(phoneNumber)
+
+    var weightInKg: Double = 53.3
+    println(weightInKg)
+
+    var isKenyan: Boolean = true
+    println(isKenyan)
+}
